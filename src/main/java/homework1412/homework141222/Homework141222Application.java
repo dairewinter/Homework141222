717692c57ef2e19fb70f28676e89c0c9c3895f2b
Daria <m-alberts@yandex.ru>
2022-12-17 12:26:54 +0300

    Initial commit

---
+++ src/main/java/homework1412/homework141222/Homework141222Application.java
@@ -0,0 +1,13 @@
+package homework1412;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework141222Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework141222Application.class, args);
+    }
+
+}
---
+++ src/test/java/homework1412/homework141222/Homework141222ApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.homework141222;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework141222ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
