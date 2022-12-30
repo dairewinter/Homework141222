88aeb36ca4bf615f099e7288b18625a15a72b2a8
Daria <m-alberts@yandex.ru>
2022-12-30 22:34:13 +0300

    дз

--- src/main/java/homework1412/controller/FilesController.java
+++ src/main/java/homework1412/controller/FilesController.java
@@ -25,29 +25,37 @@ public class FilesController {
         this.recipeFileService = recipeFileService;
     }
 
-    @GetMapping(value = "/exportrecipe", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/recipe", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<InputStreamResource> downloadRecipeFile() throws FileNotFoundException {
         File file = recipeFileService.getDataFile();
         if (file.exists()) {
             InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
-            return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).contentLength(file.length()).header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"RecipeFile.json\"").body(resource);
+            return ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .contentLength(file.length())
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=recipe.json")
+                    .body(resource);
         } else {
             return ResponseEntity.noContent().build();
         }
     }
 
-    @GetMapping(value = "/exportingredient", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/ingredient", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<InputStreamResource> downloadIngredientFile() throws FileNotFoundException {
         File file = ingredientFilesService.getDataFile();
         if (file.exists()) {
             InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
-            return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).contentLength(file.length()).header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"IngredientFile.json\"").body(resource);
+            return ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .contentLength(file.length())
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=ingredient.json")
+                    .body(resource);
         } else {
             return ResponseEntity.noContent().build();
         }
     }
 
-    @PostMapping(value = "/importrecipe", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/recipe", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Void> uploadRecipeFile(@RequestParam MultipartFile file){
         recipeFileService.removeDataFile();
         File recipeFile = recipeFileService.getDataFile();
@@ -60,7 +68,7 @@ public class FilesController {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
 
-    @PostMapping(value = "/importingredient", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/ingredient", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity <Void> uploadingredientFile(@RequestParam MultipartFile file){
         ingredientFilesService.getDataFile();
         File ingredientFile = ingredientFilesService.getDataFile();
